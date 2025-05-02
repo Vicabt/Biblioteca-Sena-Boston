cfce584b3c7c4dc44bfc15014a88e3ddfcef7cf0
Vicabt <vicab66@gmail.com>
2025-05-01 20:05:59 -0500

    fix: update firebase config

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,14 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  // Aquí necesitarás añadir tus credenciales de Firebase
-  apiKey: "AIzaSyDomn-I0rornU6A7QArxckITGndKUizemg",
-  authDomain: "biblioteca-sena-boston.firebaseapp.com",
-  projectId: "biblioteca-sena-boston",
-  storageBucket: "biblioteca-sena-boston.firebasestorage.app",
-  messagingSenderId: "947790657292",
-  appId: "1:947790657292:web:7a8c1ef063162d19727b29",
-  measurementId: "G-RXXN2NT2F4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
